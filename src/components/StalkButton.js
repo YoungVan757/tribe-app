1d9a31c77573d7b5e229a1c73e6df1dab8487f13
Christian <christian@stabmag.com>
2019-05-14 17:16:28 -0700

    removes incrementor state from buttons

--- src/components/StalkButton.js
+++ src/components/StalkButton.js
@@ -3,23 +3,10 @@ import React, { Component } from 'react';
 export default class Button extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      count: 0
-    };
-  }
-
-  incrementCounter() {
-    this.setState({
-      count: this.state.count + 1
-    });
+    this.state = {};
   }
 
   render() {
-    return (
-      <button className="board__stalk">
-        {this.props.title} {this.state.count}
-      </button>
-      
-    );
+    return <button className="board__stalk">{this.props.title}</button>;
   }
 }
