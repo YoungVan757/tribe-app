c20c2e2badc3477c8b1cefce361677bcb0cc2263
Christian <christian@stabmag.com>
2019-05-15 21:40:44 -0700

    shows prop example

--- src/components/FormField.js
+++ src/components/FormField.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 
 export default class FormField extends Component {
+  renderMyFriends() {
+    if (this.props.myFriends) {
+      const myFriendsList = Object.keys(this.props.myFriends).map(k => {
+        return <h3>{this.props.myFriends[k]}</h3>;
+      });
+
+      return myFriendsList;
+    }
+  }
+
   render() {
+    console.log(this.props.myFriends);
+
     return (
       <div className="form-field">
         <label style={{ color: this.props.color }}>{this.props.title}</label>
         <input type={this.props.inputType} />
+
+        {this.renderMyFriends()}
       </div>
     );
   }
--- src/views/Signup/index.js
+++ src/views/Signup/index.js
@@ -7,8 +7,18 @@ export default class Signup extends Component {
     return (
       <div>
         Signup
-        <FormField title="Username" inputType="text" color="red" />
-        <FormField title="Email" inputType="text" color="blue" />
+        <FormField
+          title="Username"
+          myFriends={['Bob', 'Him', 'Joanna']}
+          inputType="text"
+          color="red"
+        />
+        <FormField
+          title="Email"
+          inputType="text"
+          color="blue"
+          myFriends={['Van', 'Christine', 'Jae', 'Haesun']}
+        />
         <FormField title="Password" inputType="password" color="yellow" />
       </div>
     );
