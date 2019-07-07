7d0c66a68da244e6dfafdf6b44ad52ded1060d04
Christian <christian@stabmag.com>
2019-07-07 15:29:51 -0700

    adds window localstorage

--- src/views/Homepage/index.js
+++ src/views/Homepage/index.js
@@ -7,31 +7,101 @@ import About from '../About';
 
 export default class Homepage extends Component {
   constructor(props) {
-    super(props) ;
+    super(props);
     this.state = {
       email: '',
       password: '',
+      user: false
     };
-  };
+  }
+
+  componentDidMount() {
+    const uid = window.localStorage.getItem('tribe_uid');
+    const username = window.localStorage.getItem('tribe_username');
+
+    console.log('uid', uid, 'username', username);
+
+    if (uid && username) {
+      const user = {
+        uid,
+        username
+      };
+
+      this.setState({
+        user
+      });
+    }
+  }
 
   login() {
-  var email = document.getElementById('emailInput').value;
-  var password = document.getElementById('passwordInput').value;
-  if (email.length < 4) {
-    alert('Please enter an email address.');
-    return;
+    var email = document.getElementById('emailInput').value;
+    var password = document.getElementById('passwordInput').value;
+    if (email.length < 4) {
+      alert('Please enter an email address.');
+      return;
+    }
+    if (password.length < 4) {
+      alert('Please enter a password.');
+      return;
+    }
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+      });
   }
-  if (password.length < 4) {
-    alert('Please enter a password.');
-    return;
+
+  renderHomepageView() {
+    const { user } = this.state;
+
+    if (!user) {
+      return (
+        <React.Fragment>
+          <input
+            className="form-field"
+            id="emailInput"
+            title="Email"
+            inputType="email"
+            style={{ color: 'gold' }}
+            onChange={this.handleChange}
+            placeholder="type email"
+          />
+          <input
+            className="form-field"
+            id="passwordInput"
+            title="Password"
+            inputType="password"
+            style={{ color: 'firebrick' }}
+            onChange={this.handleChange}
+            placeholder="type password"
+          />
+          <Link to="/profile/index">
+            <button
+              className="login__link"
+              type="submit"
+              onClick={() => this.login()}
+            >
+              Log In
+            </button>
+          </Link>
+          <div className="homepage__or"> or</div>
+
+          <Link to="/signup" className="signup__link">
+            Sign Up
+          </Link>
+          <Link to="/forgotpassword" className="link">
+            Forgot password?
+          </Link>
+        </React.Fragment>
+      );
+    }
+
+    return <h1>Welcome back, {this.state.user && this.state.user.username}</h1>;
   }
-    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // ...
-  });
-}
 
   render() {
     return (
@@ -39,32 +109,7 @@ export default class Homepage extends Component {
         <div className="backyard">
           <div className="flex__container">
             <div className="homepage__heading">Tribe</div>
-              <input
-              className="form-field"
-                id="emailInput"
-                title="Email"
-                inputType="email"
-                style= {{color: "gold"}}
-                onChange={this.handleChange}
-                placeholder="type email"
-              ></input>
-              <input
-              className="form-field"
-              id="passwordInput"
-                title="Password"
-                inputType="password"
-                style= {{color: "firebrick"}}
-                onChange={this.handleChange}
-                placeholder="type password"
-              ></input>
-            <Link to="/profile/index"><button className="login__link" type="submit" onClick={this.login()}>Log In</button>
-          </Link>
-
-              <div className="homepage__or"> or</div>
-
-              <Link to="/signup" className="signup__link">Sign Up
-          </Link>
-          <Link to="/forgotpassword" className="link">Forgot password?</Link>
+            {this.renderHomepageView()}
           </div>
         </div>
       </div>
--- src/views/Signup/index.js
+++ src/views/Signup/index.js
@@ -1,80 +1,116 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import firebase from 'firebase';
 
 export default class Signup extends Component {
   constructor(props) {
     super(props);
-    this.state={
+    this.state = {
+      username: '',
       email: '',
       password: '',
-  };
-};
-
-
-signUp() {
-  var email = document.getElementById('emailInput').value;
-  var password = document.getElementById('passwordInput').value;
-  if (email.length < 4) {
-    alert('Please enter an email address.');
-    return;
+      shouldRedirect: false
+    };
   }
-  if (password.length < 4) {
-    alert('Please enter a password.');
-    return;
-  }
-  // Sign in with email and pass.
-  // [START createwithemail]
-  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // [START_EXCLUDE]
-    if (errorCode == 'auth/weak-password') {
-      alert('The password is too weak.');
-    } else {
-      alert(errorMessage);
+
+  signUp() {
+    const { username, email, password } = this.state;
+
+    if (email.length < 4) {
+      alert('Please enter an email address.');
+      return;
     }
-    console.log(error);
-    // [END_EXCLUDE]
-  });
-}
-    
+    if (password.length < 4) {
+      alert('Please enter a password.');
+      return;
+    }
+    // Sign in with email and pass.
+    // [START createwithemail]
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(userObj => {
+        const { uid } = userObj.user;
 
+        const updates = {};
+        updates[`/users/${uid}/username`] = username;
+        updates[`/users/${uid}/email`] = email;
+        updates[`/foods/banana/eaten`] = true;
+
+        firebase
+          .database()
+          .ref()
+          .update(updates)
+          .then(() => {
+            // Authenticate the user in the APP, and let them in the door..
+            window.localStorage.setItem('tribe_uid', uid);
+            window.localStorage.setItem('tribe_username', username);
+
+            this.setState({
+              shouldRedirect: true
+            });
+          });
+
+        // Where we save any additional info about our new user..
+      })
+      .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // [START_EXCLUDE]
+        if (errorCode == 'auth/weak-password') {
+          alert('The password is too weak.');
+        } else {
+          alert(errorMessage);
+        }
+        console.log(error);
+        // [END_EXCLUDE]
+      });
+  }
 
   render() {
-    return(
+    if (this.state.shouldRedirect) {
+      return <Redirect to="/" />;
+    }
+
+    return (
       <div claasName="container">
         <div className="backyard">
           <div className="flex__container">
-              <div className="heading">Sign Up</div>
-              <input
-                className='form-field'
-                title="Username"
-                inputType="text"
-                style={{color: 'gold'}}
-                placeholder="type username"
-              ></input>
-              <input
-              className='form-field'
-                id='emailInput'
-                title="Email"
-                inputType="email"
-                color="darkgreen"
-                style={{color: 'firebrick'}}
-                placeholder="type email"
-              ></input>
-              <input
-              className='form-field'
-                id='passwordInput'
-                title="Password"
-                inputType='password'
-                style={{color: 'darkgreen'}}
-                placeholder="type password"
-              ></input>
-              <Link to="/"><button onClick={(e) => this.signUp()} title="Submit" borderColor="black">Sign Up</button></Link>
+            <div className="heading">Sign Up</div>
+
+            <input
+              onChange={e => this.setState({ username: e.target.value })}
+              className="form-field"
+              style={{ color: 'gold' }}
+              placeholder="type username"
+            />
+            <input
+              className="form-field"
+              onChange={e => this.setState({ email: e.target.value })}
+              inputType="email"
+              color="darkgreen"
+              style={{ color: 'firebrick' }}
+              placeholder="type email"
+            />
+            <input
+              onChange={e => this.setState({ password: e.target.value })}
+              className="form-field"
+              type="password"
+              style={{ color: 'darkgreen' }}
+              placeholder="type password"
+            />
+
+            <button
+              onClick={e => this.signUp()}
+              title="Submit"
+              borderColor="black"
+            >
+              Sign Up
+            </button>
           </div>
         </div>
       </div>
     );
-  }};
+  }
+}
